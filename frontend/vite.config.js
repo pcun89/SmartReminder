2eabfb3cfcd94a641bbfdc84602b57dd41dd2f39
pcun89 <phillippcun89@gmail.com>
2026-01-05 11:53:16 -0600

    bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/SmartRemindeR/",
+    base: "/SmartReminder/",
     plugins: [react()],
 });
